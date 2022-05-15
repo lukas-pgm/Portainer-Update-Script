f40a798262d8f82ed2a0ce6d3fa8526b94df28d8
Lukas <56029347+lukas-pgm@users.noreply.github.com>
2022-05-15 14:04:07 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 # Script written by lukas-pgm
-echo "Checking compatibility..."
+echo "[Updater] Checking compatibility..."
 if [ -x "$(command -v docker)" ]; then
-    echo "Docker installation found."
-    echo ""
-    echo "Trying to stop portainer container..."
+    echo "[Updater] Docker installation found."
+    echo "[Updater] Trying to stop portainer container..."
     sudo docker stop portainer
-    echo "Completed."
-    echo "Trying to remove old portainer container..."
+    echo "[Updater] Completed."
+    echo "[Updater] Trying to remove old portainer container..."
     sudo docker rm portainer
-    echo "Completed."
-    echo "Trying to pull the new version of portainer..."
+    echo "[Updater] Completed."
+    echo "[Updater] Trying to pull the new version of portainer..."
     sudo docker pull portainer/portainer-ce:latest
-    echo "Completed."
-    echo "Trying to launch the newest version of portainer..."
+    echo "[Updater] Completed."
+    echo "[Updater] Trying to launch the newest version of portainer..."
     sudo docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-    echo "Completed."
+    echo "[Updater] Completed."
     echo ""
-    echo ""
-    echo "Update finished!"
+    echo "[Updater] Update finished!"
 else
+    echo ""
     echo "No docker installation found!"
 fi
